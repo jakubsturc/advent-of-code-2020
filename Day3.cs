73803f397df7cd113c349b01e2fcf7fe5b8ba272
Jakub Šturc <jakub.sturc@gmail.com>
2020-12-03 12:04:52 +0100

    Add day 3 part 2

--- Day3.cs
+++ Day3.cs
@@ -12,14 +12,29 @@ namespace JakubSturc.AdventOfCode2020
         {
             var input = Input.Read(day: 3);
             var map = new Map(input);
-            return Sloop(map);
+            return Sloop(map, Δ: (row: 1, col: 3));
         }
 
-        public static int Sloop(Map map)
+        public static long Part2()
+        {
+            var input = Input.Read(day: 3);
+            var map = new Map(input);
+            var res = 1L;
+            
+            res *= Sloop(map, Δ: (row: 1, col: 1));
+            res *= Sloop(map, Δ: (row: 1, col: 3));
+            res *= Sloop(map, Δ: (row: 1, col: 5));
+            res *= Sloop(map, Δ: (row: 1, col: 7));
+            res *= Sloop(map, Δ: (row: 2, col: 1));
+
+            return res;
+        }
+
+        public static int Sloop(Map map, (int row, int col) Δ)
         {
             var res = 0; // number of hit trees
 
-            for (int row=0, col=0; row<map.Height; row+=1, col+=3)
+            for (int row=0, col=0; row<map.Height; row+=Δ.row, col+= Δ.col)
             {
                 if (map[row, col] == '#') res++;
             }
--- Program.cs
+++ Program.cs
@@ -5,3 +5,4 @@ System.Console.WriteLine($"Day 1, Part 2: {Day1.Part2()}");
 System.Console.WriteLine($"Day 2, Part 1: {Day2.Part1()}");
 System.Console.WriteLine($"Day 2, Part 2: {Day2.Part2()}");
 System.Console.WriteLine($"Day 3, Part 1: {Day3.Part1()}");
+System.Console.WriteLine($"Day 3, Part 2: {Day3.Part2()}");
